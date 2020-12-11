26e80ed9a6e663aa9add553ef5a5dcf99faf3f64
ggramaize <geoffroy+dev@gramaize.eu>
2020-12-11 15:14:43 +0100

    app.js: continue implementing browser behaviour. Add redirections.

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,6 +18,8 @@ class GpxBrowser {
 		
 		// Browsing flag status
 		this.is_browsing = false;
+
+		this.last_valid_url = '';
 		
 		// Instanciate our XHR object
 		this.xhr = new XMLHttpRequest();
@@ -26,6 +28,9 @@ class GpxBrowser {
 		this.xhr.onreadystatechange = e => this.xhrOnReadyStateChange();
 		this.xhr.ontimeout = e => this.xhrOnTimeout();
 		
+		// Set the previous from local storage if a value is found
+		this.last_valid_url = 'gemini://gemini.lan/';
+
 		// Bind button events to methods
 		//document.getElementById('ui-prev').addEventListener( 'click', this.);
 		//document.getElementById('ui-next').addEventListener( 'click', this.);
@@ -49,11 +54,113 @@ class GpxBrowser {
 	
 	browse()
 	{
+		// Generate the proxy URL from the document URL
 		let requestedUrl = document.getElementById('url-bar').value;
-		requestedUrl = 'https://test-ecdsa.luthienstar.net/gpx/cprox.php?url=' + encodeURI( requestedUrl);
+
+		requestedUrl = GpxBrowser.cleanUrl( requestedUrl);
+		document.getElementById('url-bar').value = requestedUrl;
+
+		requestedUrl = document.URL.replace( /^((?:[^\/]*\/)+).+$/, '$1') + 'cprox.php?url=' + encodeURI( requestedUrl);
 		this.queryProxy( requestedUrl);
 	}
-	
+
+	browseFromLink(e)
+	{
+		// Reset the redirection TTL
+		this.redirect_count = 5;
+
+		// Intercept the page behaviour
+		e.preventDefault();
+
+		this.loadPage( e.target.attributes.href.value);
+	}
+
+	loadPage( linkValue)
+	{
+		let currentPageUrl = document.getElementById('url-bar').value;
+
+		// Absolute gemini link
+		if( linkValue.match( /^gemini:\/\/.*/) != null )
+		{
+			document.getElementById('url-bar').value = linkValue;
+			this.browse();
+			return;
+		}
+
+		// Absolute gopher link
+		if( linkValue.match( /^gopher:\/\/.*/) != null )
+		{
+			// NOOP for the moment, browse once proxy works
+		}
+
+		// HTTP/HTTPS link
+		if( linkValue.match( /^https?:\/\/.*/) != null )
+		{
+			// TODO: Open in new tab
+			return;
+		}
+
+		// Foreign protocol
+
+
+		// Same page
+		if( linkValue.match( /^#.*/) != null )
+			return;
+
+		// Canonical URL without the protocol
+		if( linkValue.match( /^\/\/.*/) != null )
+		{
+			document.getElementById('url-bar').value = 'gemini:' + linkValue;
+			this.browse();
+			return;
+		}
+
+		// Absolute URL
+		if( linkValue.match( /^\/.*/) != null )
+		{
+			let currentHost = document.getElementById('url-bar').value.replace( /^(gemini:\/\/[^\/\r\n]+)\/?.*$/, '$1');
+			document.getElementById('url-bar').value = currentHost + linkValue;
+			this.browse();
+			return;
+		}
+
+		// Relative URL
+		let currentHost = document.getElementById('url-bar').value.replace( /^((?:[^\/\n]*\/)+).*$/, '$1');
+		document.getElementById('url-bar').value = currentHost + linkValue;
+		this.browse();
+	}
+
+	static cleanUrl(url)
+	{
+		let matches = url.match( /^(gemini:\/\/[^\/\n\r]+\/)((?:[^\/\n\r]+\/)+)(.*)$/);
+		if( matches == null )
+			return url;
+
+		let path = '';
+		let elts = matches[2].split('/');
+
+		for( var i=0; i<elts.length; ++i)
+		{
+			if( elts[i] == '' || elts[i] == '.' )
+				continue;
+
+			if( elts[i] == '..')
+			{
+				if( path == '')
+					continue;
+
+				// Remove the last directory
+				path = path.replace( /^((?:[^\/\n\r]+\/)*)[^\/]+\/$/, '$1');
+				continue;
+			}
+
+			path += elts[i] + '/';
+			console.log(path);
+		}
+
+		return matches[1] + path + matches[3];
+	}
+
 	queryProxy( url)
 	{
 		// Set the navigation status
@@ -69,6 +176,7 @@ class GpxBrowser {
 	{
 		// Parse the proxy reply
 		let reply = JSON.parse( r);
+		let replyHeader = reply.header.split( ' ', 2);
 		
 		// TODO: check the URL's host part matches CN or one of the subjectAltName, if applicable.
 		// TODO: DANE validation
@@ -88,12 +196,42 @@ class GpxBrowser {
 		}
 		// */
 		
-		// TODO: if redirect, decrement the redirection counter, then query proxy for the new target, if applicable and exit.
-		
+		// Redirection
+		if( replyHeader[0] >= 30 && replyHeader[0] <= 39)
+		{
+			if( --this.redirect_count < 0 )
+			{
+				// TODO: display an error screen if we have a redirection loop
+
+				// Restore the last valid URL we had
+				document.getElementById('url-bar').value = this.last_valid_url;
+				alert('redir loop!');
+				return;
+			}
+
+			if( replyHeader[0] == 31 )
+			{
+				// TODO: Permanent redirection operations
+			}
+
+			// TODO: handle cross-protocol warning
+
+			this.loadPage( replyHeader[1]);
+			return;
+		}
+
 		// Update the page body
 		let pageBody = document.getElementsByClassName('server_resp')[0];
 		pageBody.innerHTML = reply.content;
+	
+		// Intercept link click events
+		let links = pageBody.getElementsByTagName('a');
+		for( let i=0; i<links.length; ++i)
+			links.item(i).addEventListener( 'click', e => this.browseFromLink(e));
 		
+		// Store this value as our last anchor point, in case a redirection loop happens
+		this.last_valid_url = document.getElementById('url-bar').value;
+
 		this.updateBrowsingStatus( false);
 	}
 	
@@ -102,6 +240,8 @@ class GpxBrowser {
 		// TODO
 		alert('Not implemented.');
 	}
+
+
 	
 	updateBrowsingStatus( flag)
 	{
@@ -135,6 +275,10 @@ class GpxBrowser {
 	
 	urlbarOnKeyPress(e)
 	{
+		// Reset the redirection TTL
+		this.redirect_count = 5;
+
+		// Browse if the return key has been pressed
 		if( e.keyCode == 13 )
 		{
 			this.browse();
